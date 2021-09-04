fbfde6acafa6db069f14c0835773933411bfcaf3
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-09-04 19:34:48 +0200

    Dark mode button test

---
+++ assets/mode.js
@@ -0,0 +1,11 @@
+function mode() {
+
+    var body = document.getElementsByTagName('body')[0].style
+
+    if (body = 'background-color: black') {
+        console.log('Darkmode')
+    }
+    else {
+        console.log('Lightmode')
+    }
+}
