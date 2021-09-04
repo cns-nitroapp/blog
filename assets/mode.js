6d1f9c40e6b3431e6a3bb78201c4d212187a1e51
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-09-04 19:53:35 +0200

    Fix post

--- assets/mode.js
+++ assets/mode.js
@@ -2,6 +2,8 @@ function mode() {
 
     var body = document.getElementsByTagName('body')[0].style
 
+    console.log(body)
+
     if (body = 'background-color: black') {
         console.log('Darkmode')
     }
