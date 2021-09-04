92e3b9702bf0c45be4ab7fb99fbe8b897e6e36b9
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-09-04 20:24:54 +0200

    Update mode.js

--- assets/mode.js
+++ assets/mode.js
@@ -1,15 +1,26 @@
+const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+function enableDarkmode() {
+    body = 'black';
+}
+
+function enableLightmode() {
+    body = '#fff';
+}
+
 function mode() {
 
     var body = document.body.style.background
 
-    console.log(body)
+    if (userPrefersDark) {
+        console.log('Previously Darkmode - Changed to Lightmode');
+        enableLightmode();
+    }
+    else if (userPrefersLight) {
+        console.log('Previously Lightmode - Changed to Darkmode');
+        enableDarkmode();
+    }
 
-    if (body == '') {
-        console.log('Lightmode')
-        body = "black"
-    }
-    else if (body == 'black') {
-        console.log('Darkmode')
-        document.body.style.background = "#fff"
-    }
 }
