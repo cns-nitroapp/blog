7ed8e99179148f622ad31b6adcebedd002d33c1a
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-09-05 15:03:57 +0200

    Update mode.js

--- assets/mode.js
+++ assets/mode.js
@@ -4,11 +4,11 @@ const setColorScheme = e => {
   if (e.matches) {
     // Dark
     console.log('Darkmode')
-    document.getElementById("demo").innerHTML = "Dark"
+    document.getElementById("demo").innerHTML = "Darkmode"
   } else {
     // Light
     console.log('Lightmode')
-    document.getElementById("mode").innerHTML = "Light"
+    document.getElementById("mode").innerHTML = "Lightmode"
   }
 }
 
@@ -17,12 +17,10 @@ colorSchemeQueryList.addListener(setColorScheme);
 
 function enableDarkmode() {
     document.body.style.background = 'black';
-    ifLight('set', false);
 }
 
 function enableLightmode() {
     document.body.style.background = '#fff';
-    ifLight('set', true)
 }
 
 function mode() {
